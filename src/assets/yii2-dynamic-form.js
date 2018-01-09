50616f2f4e523c295e6bf09186316c637a4997ef
shashi53 <kumarshashi53@gmail.com>
2018-01-09 13:14:32 +0530

    Update yii2-dynamic-form.js

--- src/assets/yii2-dynamic-form.js
+++ src/assets/yii2-dynamic-form.js
@@ -86,9 +86,9 @@
         });
 
         // remove "error/success" css class
-        var yiiActiveFormData = $('#' + widgetOptions.formId).yiiActiveForm('data');
-        $template.find('.' + yiiActiveFormData.settings.errorCssClass).removeClass(yiiActiveFormData.settings.errorCssClass);
-        $template.find('.' + yiiActiveFormData.settings.successCssClass).removeClass(yiiActiveFormData.settings.successCssClass);
+        //var yiiActiveFormData = $('#' + widgetOptions.formId).yiiActiveForm('data');
+        //$template.find('.' + yiiActiveFormData.settings.errorCssClass).removeClass(yiiActiveFormData.settings.errorCssClass);
+        //$template.find('.' + yiiActiveFormData.settings.successCssClass).removeClass(yiiActiveFormData.settings.successCssClass);
 
         return $template;
     };
